94e09e5353b99331d319b99934384673cb1d1eb6
cspatil8 <patil.chinmay8@gmail.com>
2017-07-18 22:54:18 -0400

    First commit

---
+++ CookingApp/Ingredient.swift
@@ -0,0 +1,13 @@
+//
+//  Ingredient.swift
+//  CookingApp
+//
+//  Created by Chinmay Patil on 7/18/17.
+//  Copyright © 2017 Chinmay Patil. All rights reserved.
+//
+
+import Foundation
+
+class Ingredient {
+    
+}
